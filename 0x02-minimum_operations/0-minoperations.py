67daed9085e9fc94a6c4974ecf5e1704825bf7a8
1xcodex <yishak.ab1216@gmail.com>
2023-01-11 21:55:29 +0300

    edite

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-"""
-Module 0x02-minimum_operations
-"""
+'''The minimum operations coding challenge.
+'''
 
 
-def minOperations(n: int) -> int:
-    """
-    Returns fewest number of operations
-    needed to result in exaclty n H
-    characters in the file
-    """
-    counter = 0
-
-    if n <= 1:
-        return counter
-
-    for i in range(2, n + 1):
-        while (0 == n % i):
-            counter = counter + i
-            n = n / i
-            if n < i:
-                break
-    return counter
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
